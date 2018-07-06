f8fe73723375350ece49c6c4911b5f38fb51b67a
Talia Asghar Mirza <talia@marketlytics.com>
2018-07-06 17:54:05 +0500

    Merge conflicts

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
-
+import { Checkbox, Button } from 'react-bootstrap'
 
 class TodoItem extends React.Component {
     render() {
         return (
             <div>
-                <input type="checkbox" checked ={this.props.todo.isChecked ? "checked" : ""} onChange ={() => this.props.selectedCallback(this.props.todo)}/>
-                <p>{this.props.todo.name}</p>
-                <button onClick = {() => this.props.removeCallback(this.props.todo)}>X</button>
+                <Checkbox style={{padding: 5, border : '1px solid black', width: 300, margin: '0 auto'}} type="checkbox" checked ={this.props.todo.isChecked ? "checked" : ""} onChange ={() => this.props.selectedCallback(this.props.todo)}>
+                <p style={{padding: 20, display: 'inline'}}>{this.props.todo.name}</p>
+                <Button onClick = {() => this.props.removeCallback(this.props.todo)}>X</Button>
+                </Checkbox>
             </div>
         )
     } 
